fc17ca25a69d6ac06b335fdef86b24d3d18f62b5
Ethel <ethel.maria.benko@gmail.com>
2020-08-21 16:29:20 +0200

    Save work

--- JS.js
+++ JS.js
@@ -1,3 +1,4 @@
+
 /*click handle*/
 document.querySelector('.card-btn').addEventListener
 ('click', ()=> {
@@ -6,13 +7,13 @@ document.querySelector('.card-btn').addEventListener
 
     document.querySelector('.Initial')
         .style.cssText = 'opacity: 1; visibility: visible';
-    document.querySelector('.in')
+    document.querySelector('.contact-icons')
         .style.cssText = 'opacity: 1; visibility: visible';
     document.querySelector('.x-btn')
         .style.cssText = 'opacity: 1; visibility: visible';
 
-    document.querySelector('.card-wrapper');
-
+    document.querySelector('.card-wrapper')
+        .style.background= '#292929';
 
 });
 
@@ -24,37 +25,42 @@ document.querySelector('.x-btn').addEventListener(
 
     document.querySelector('.Initial')
     .style.cssText = 'opacity: 0; visibility: hidden';
-    document.querySelector('.in')
+    document.querySelector('.contact-icons')
     .style.cssText = 'opacity: 0; visibility: hidden';
     document.querySelector('.x-btn')
     .style.cssText = 'opacity: 0; visibility: hidden';
 
     document.querySelector('.card-wrapper')
-    .style.cssText = 'background: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("Forest.jpg") no-repeat center;';
+    .style.cssText = 'background: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("pictures/Forest.jpg") no-repeat center;';
 
 });
 
 /*click Full CV*/
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function(){
+
     document.querySelector('.full-cv-btn').addEventListener
 ('click', ()=> {
+
     document.querySelector('.card-wrapper')
     .style.display = 'none';
 
+    document.querySelector('body')
+    .style.cssText = 'overflow: auto';
+
     document.querySelector('.full-cv')
-    .style.cssText = 'opacity: 1; visibility: visible';
+    .style.cssText = 'opacity: 1; visibility: visible'
+;
 
     document.querySelector('.slideshow')
     .style.animation = 'slideshow 20s infinite';
-});
 
 });
+});
 
 
-
-/*click back-btn*/
-document.querySelector('.back-btn').addEventListener(
+/*click logo to back*/
+document.querySelector('.logo').addEventListener(
     'click', ()=> {
     document.querySelector('.full-cv')
     .style.display = 'none';
@@ -64,8 +70,63 @@ document.querySelector('.back-btn').addEventListener(
 
     document.querySelector('.card-wrapper')
     .style.background= '#292929';
+});
+
+/*click back-btn to top*/
+
+/*document.querySelector('.back-btn').addEventListener(
+    'click', ()=> {
+    window.scrollTo(screenTop);
 
 });
+
+$('html, body').animate({
+    scrollTop: $(".back-btn").offset().top
+}, 0);});*/
+
+const btnScroll = document.querySelector('.back-btn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnScroll.style.display = "block";
+    } else {
+        btnScroll.style.display = "none";
+    }
+}
+
+btnScroll.addEventListener('click', function() {
+   window.scrollTo({
+       top: 0,
+       left: 0,
+      behavior: 'smooth'
+   });
+});
+
+
+
+/*
+//Get the button:
+mybutton = document.getElementsByClassName("back-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnScroll.style.display = "block";
+    } else {
+        btnScroll.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}*/
+
 
 /*click Short Introduction*/
 document.querySelector('.introduction-btn').addEventListener
@@ -74,6 +135,7 @@ document.querySelector('.introduction-btn').addEventListener
     document.querySelector('.short-intro')
     .style.cssText = 'opacity: 1; visibility: visible';
 });
+
 
 /*click back-btn2*/
 document.querySelector('.back-btn2').addEventListener(
@@ -86,7 +148,29 @@ document.querySelector('.back-btn2').addEventListener(
 
 });
 
-//BLACKROCK DETAILS:
+/*click email icon*/
+document.querySelector('.email').addEventListener
+('click', ()=> {
+
+    document.querySelector('.email-popup')
+        .style.cssText = 'opacity: 1; visibility: visible';
+});
+
+
+/*click back-btn8*/
+document.querySelector('.back-btn8').addEventListener(
+    'click', ()=> {
+        document.querySelector('.email-popup')
+            .style.display = 'none';
+
+        document.querySelector('.card-wrapper')
+            .style.background= '#292929';
+
+    });
+
+
+
+    //BLACKROCK DETAILS:
 
 document.querySelector('#blk-det').addEventListener
 ('click', ()=> {
@@ -100,8 +184,7 @@ document.querySelector('.back-btn3').addEventListener(
     .style.display = 'none';
 });
 
-
-//VODAFONE DETAILS:
+    //VODAFONE DETAILS:
 
 document.querySelector('#vf-det').addEventListener
 ('click', ()=> {
@@ -115,7 +198,7 @@ document.querySelector('.back-btn4').addEventListener(
     .style.display = 'none';
 });
 
-//ICE DETAILS:
+    //ICE DETAILS:
 
 document.querySelector('#ice-det').addEventListener
 ('click', ()=> {
@@ -129,7 +212,7 @@ document.querySelector('.back-btn5').addEventListener(
     .style.display = 'none';
 });
 
-//ABBOTT DETAILS:
+    //ABBOTT DETAILS:
 
 document.querySelector('#abbott-det').addEventListener
 ('click', ()=> {
@@ -143,7 +226,7 @@ document.querySelector('.back-btn6').addEventListener(
     .style.display = 'none';
 });
 
-//NU SKIN DETAILS:
+    //NU SKIN DETAILS:
 
 document.querySelector('#nuskin-det').addEventListener
 ('click', ()=> {
